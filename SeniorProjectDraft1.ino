5d91c6ca1aba80b203c7e53de979d5687fe1b885
Grant Madson <grantmadson@gmail.com>
2018-03-25 19:48:51 -0600

    Added tested code.
    Deleted the old framework and added in the code which I wrote and tested for the parts I already have. So far, there is an arduino, the OLED screen and two force sensing resistors. The code currently displays the adafruit symbol, Sen. Proj. 2018, and then the net values from the force sensors. I have not yet confirmed what units the force sensors display.

--- SeniorProjectDraft1.ino
+++ SeniorProjectDraft1.ino
@@ -1,65 +1,72 @@
-#include <SPI.h>
-#include <SD.h>
-#include <LiquidCrystal.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+#define NUMFLAKES 10
+#define XPOS 0
+#define YPOS 1
+#define DELTAY 2
+
+
+#define LOGO16_GLCD_HEIGHT 16 
+#define LOGO16_GLCD_WIDTH  16 
+const static unsigned char PROGMEM logo16_glcd_bmp[] =
+{ B00000000, B11000000,
+  B00000001, B11000000,
+  B00000001, B11000000,
+  B00000011, B11100000,
+  B11110011, B11100000,
+  B11111110, B11111000,
+  B01111110, B11111111,
+  B00110011, B10011111,
+  B00011111, B11111100,
+  B00001101, B01110000,
+  B00011011, B10100000,
+  B00111111, B11100000,
+  B00111111, B11110000,
+  B01111100, B11110000,
+  B01110000, B01110000,
+  B00000000, B00110000 };
+
+  const int POSFORCE = 0;
+  const int NEGFORCE = 1;
+  int NETFORCE;
+  int POS;
+  int NEG;
 
 void setup() {
   // put your setup code here, to run once:
-  const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
-LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
-// set up the LCD's number of columns and rows:
-  lcd.begin(16, 2);
-  // Print a message to the LCD.
-  lcd.print("hello, world!");
-   Serial.begin(9600);
-  while (!Serial) {
-    ; // wait for serial port to connect. Needed for native USB port only
-  }
+  Serial.begin(9600);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3C (for the 128x32)
+  display.display(); // show splashscreen
+  delay(500);
+  display.clearDisplay();   // clears the screen and buffer
+  display.setTextSize(2);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("Sen. Proj. 2018");
+  display.display();
+  delay(1000);
 
-
-  Serial.print("Initializing SD card...");
-
-  // see if the card is present and can be initialized:
-  if (!SD.begin(chipSelect)) {
-    Serial.println("Card failed, or not present");
-    // don't do anything more:
-    return;
-  }
-  Serial.println("card initialized.");
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
- // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
-  lcd.setCursor(0, 1);
-  // print the number of seconds since reset:
-  lcd.print(millis() / 1000);
-   // make a string for assembling the data to log:
-  String dataString = "";
+  display.clearDisplay();
+  display.setCursor(0,0);
+  display.setTextSize(5);
+  POS = analogRead(POSFORCE);
+  NEG = analogRead(NEGFORCE) * -1;
+  NETFORCE = POS + NEG;
+  display.print(NETFORCE);
+  display.display();
+  delay(50);
+  Serial.print(analogRead(NETFORCE), "/n/n");
+  Serial.print(analogRead(NEGFORCE), "/n/n");
+  Serial.println(analogRead(POSFORCE));
 
-  // read two sensors and append to the string:
-  for (int analogPin = 0; analogPin < 2; analogPin++) {
-    int sensor = analogRead(analogPin);
-    dataString += String(sensor);
-    if (analogPin < 2) {
-      dataString += ",";
-    }
-  }
-
-  // open the file. note that only one file can be open at a time,
-  // so you have to close this one before opening another.
-  File dataFile = SD.open("datalog.txt", FILE_WRITE);
-
-  // if the file is available, write to it:
-  if (dataFile) {
-    dataFile.println(dataString);
-    dataFile.close();
-    // print to the serial port too:
-    Serial.println(dataString);
-  }
-  // if the file isn't open, pop up an error:
-  else {
-    Serial.println("error opening datalog.txt");
-  }
 }
